2314494cc9937afe2ed4df8c3b4922ceea3b7d34
maralt2001 <markus.altendorfer@kabelmail.de>
2019-06-23 18:54:43 +0200

    set TimeFormat

--- src/resolvers/ResolverLogger.ts
+++ src/resolvers/ResolverLogger.ts
@@ -1,7 +1,4 @@
 import {Log} from '../models/log'
-import {ILogItem} from '../models/ilog'
-import { log } from 'util';
-import { addErrorLoggingToSchema } from 'graphql-tools';
 const mongoose = require ('mongoose')
 
 module.exports = {
@@ -9,26 +6,30 @@ module.exports = {
     GetAllLogs: async () => {
 
         let findlogs:Array<any> = await Log.find({}, {"__v": false})
-        return  findlogs.map(logItem => {return {...logItem._doc, dateTime: logItem.dateTime.toUTCString()} } ) //change date Format to UTC String
+        return  findlogs.map(logItem => {return {...logItem._doc, dateTime: setTimeFormat(logItem.dateTime)} } )
         
     },
     createLog: async (args:any) => {
-
         
         const newLogItem = new Log({
             _id: new mongoose.Types.ObjectId(),
-            DateTime: new Date(),
-            RequestedUrl: args.logInput.requestedUrl,
-            RequestedProtocol: args.logInput.requestedProtocol,
-            RequestedMethod: args.logInput.requestedMethod
+            dateTime: new Date(),
+            requestedUrl: args.logInput.requestedUrl,
+            requestedProtocol: args.logInput.requestedProtocol,
+            requestedMethod: args.logInput.requestedMethod
 
         })
 
         let result:any= await newLogItem.save()
         let {...properties} = result._doc
-        properties.dateTime  = properties.dateTime.toUTCString()
-        
+        properties.dateTime = setTimeFormat(properties.dateTime)
         return(properties)
 
     }
+
+    
+}
+
+function setTimeFormat (timeItem:Date) {
+      return timeItem.toLocaleString()  
 }
